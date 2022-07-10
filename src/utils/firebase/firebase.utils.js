0b7b11f3df161d5496da59d21a0cad120b9179a0
Tejas Tripathi <tejas.tripathi.741@gmail.com>
2022-07-11 00:02:09 +0530

    initialized firebase and navigation

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCP9MXuJeFZ681RJS85zh8-1uHOp04wg0w",
+    authDomain: "react-crwn-2c7e5.firebaseapp.com",
+    projectId: "react-crwn-2c7e5",
+    storageBucket: "react-crwn-2c7e5.appspot.com",
+    messagingSenderId: "404805341164",
+    appId: "1:404805341164:web:4bdab806f40beaf91bbac9"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
