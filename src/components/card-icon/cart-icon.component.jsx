0b05b7d86622115489de47f752f84a77354806eb
Tejas Tripathi <tejas.tripathi.741@gmail.com>
2023-02-11 01:01:28 +0530

    migrated cart context to redux

--- src/components/card-icon/cart-icon.component.jsx
+++ src/components/card-icon/cart-icon.component.jsx
@@ -1,12 +1,16 @@
-import { useContext } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { CartContext } from '../../context/cart.context';
-
+import { selectIsCartOpen, selectCartCount } from "../../store/cart/cart.selector";
+import { setIsCartOpen } from "../../store/cart/cart.action";
 import { ShoppingIcon, CartIconContainer, ItemCount } from './cart-icon.styles.jsx';
 
 const CartIcon = () => {
-    const {isCartOpen, setIsCartOpen, cartCount} = useContext(CartContext);
-    const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
+    // const {isCartOpen, setIsCartOpen, cartCount} = useContext(CartContext);
+    const dispatch = useDispatch();
+    const isCartOpen = useSelector(selectIsCartOpen);
+    const cartCount = useSelector(selectCartCount);
+
+    const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen))
 
     return (
         <CartIconContainer onClick={toggleIsCartOpen}>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useContext } from "react";
+import { Fragment } from "react";
 import { Outlet } from 'react-router-dom';
 import { useSelector } from "react-redux";
 
@@ -6,7 +6,7 @@ import CartIcon from "../../components/card-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { ReactComponent as Crwn } from '../../assets/crown.svg';
 
-import { CartContext } from '../../context/cart.context';
+import { selectIsCartOpen } from "../../store/cart/cart.selector";
 import { selectCurrentUser } from "../../store/user/user.selector";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
@@ -16,7 +16,7 @@ import { NavigationContainer, LogoContainer, NavLinks, NavLink} from './navigati
 
 const Navigation = () => {
     const currentUser = useSelector(selectCurrentUser)
-    const { isCartOpen } = useContext(CartContext);
+    const  isCartOpen  = useSelector(selectIsCartOpen)
 
     return (
         <Fragment>
